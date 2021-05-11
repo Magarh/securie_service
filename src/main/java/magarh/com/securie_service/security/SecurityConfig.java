01cf8d16111ac28d3ba641496c75719334fd6a7d
mon-désir Tsilouoni <mondesirtsilouoni@gmail.com>
2021-05-11 15:22:57 +0200

    Commit pour la configuration de la sécurité

--- src/main/java/magarh/com/securie_service/security/SecurityConfig.java
+++ src/main/java/magarh/com/securie_service/security/SecurityConfig.java
@@ -28,9 +28,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	   http.csrf().disable();
-	   http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-	   http.authorizeRequests().antMatchers("/login/**", "/register/**").permitAll();
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().antMatchers("/login/**", "/register/**").permitAll();
 	   http.authorizeRequests().antMatchers("/appUsers/**", "/appRoles/**").hasAuthority("ADMIN");
 	   http.authorizeRequests().anyRequest().authenticated();
 	   http.addFilter(new JWTAuthenticationFilter(authenticationManager()));	
